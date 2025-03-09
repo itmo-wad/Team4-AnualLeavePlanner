ce14e56d8f924e5c118db7b6f00d9277bed15224
Brooo <brooomaster@gmail.com>
2025-03-09 17:12:47 +0300

    Add leave details modal in manager dashboard

--- app/calendar.py
+++ app/calendar.py
@@ -35,16 +35,20 @@ def calendar_data():
                 "title": f"{user['surname']} {user['firstname']}",
                 "start": leave_request['start_date'],
                 "end": end_date.strftime("%Y-%m-%d"),
-                "backgroundColor": user.get('color', 'blue')
+                "backgroundColor": user.get('color', 'blue'),
+                # Info so we can grab it on click
+                "id": str(leave_request['_id']),
+                "status": leave_request['status'],
+                "email": user['email']
             })
 
         # Changes color for both overlapping leaves, O(n^2) complexity
-        # for i in range(len(data)):
-        #     for j in range(i + 1, len(data)):
-        #         if data[i]['start'] <= data[j]['start'] <= data[i]['end']:
-        #             data[i]['backgroundColor'] = "red"
-        #             data[j]['backgroundColor'] = "red"
-        data = check_overlapping_leaves(data)
+        for i in range(len(data)):
+            for j in range(i + 1, len(data)):
+                if data[i]['start'] <= data[j]['start'] <= data[i]['end']:
+                    data[i]['borderColor'] = "red"
+                    data[j]['borderColor'] = "red"
+        # data = check_overlapping_leaves(data)
         return jsonify(data)
 
 def check_overlapping_leaves(leave_requests):
@@ -57,7 +61,7 @@ def check_overlapping_leaves(leave_requests):
 
     for leave in leave_requests:
         if prev_end and leave['start'] <= prev_end:
-            leave['backgroundColor'] = "red"
+            leave['borderColor'] = "red"
         prev_end = max(prev_end, leave['end']) if prev_end else leave['end']
 
     return leave_requests
